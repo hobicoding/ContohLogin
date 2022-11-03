3e88739e120bf27a5027896c4d74fa35168dbc72
Agung Kurniawan <agung.kurniawan@bsgroup.id>
2022-11-03 09:43:17 +0700

    Contoh Form Login

--- ContohLogin/Form1.cs
+++ ContohLogin/Form1.cs
@@ -11,9 +11,26 @@ namespace ContohLogin
 {
     public partial class Form1 : Form
     {
+        string Username = "admin";
+        string Password = "123456";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            if(tbUsername.Text.ToUpper()==Username.ToUpper() && tbPassword.Text==Password)
+            {
+                Form2 frm = new Form2();
+                frm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Username or Password wrong!", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
         }
     }
 }
